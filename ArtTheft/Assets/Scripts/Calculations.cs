e37cd30c941b45f12aa272c37f8e9054409fcb4a
Jonathan Clement <blowdacon@gmail.com>
2020-04-19 19:03:16 -0400

    pushed what i have now

---
+++ ArtTheft/Assets/Scripts/Calculations.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculations : MonoBehaviour
+{
+    public float Val1 = 100.0f;
+    public float Val2;
+    private float Final;
+
+    private void Start()
+    {
+        Final = Val1 - Val2;
+        if (Final > 0)
+        {
+            Debug.Log("out of 10 of your choices, You have stolen " + Final + " Art pieces!");
+        }
+    }
+    void Update()
+    {
+       
+    }
+}
