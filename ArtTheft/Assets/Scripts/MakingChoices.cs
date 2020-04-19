0ceba218f7027d3771e9114580b1199b5b325b39
Joel Gonzalez <jg22320@gmail.com>
2020-04-19 19:59:08 -0400

    Added New Script of Choices

--- ArtTheft/Assets/Scripts/MakingChoices.cs
+++ ArtTheft/Assets/Scripts/MakingChoices.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MuiltpleChoice : MonoBehaviour {
+public class MakingChoices : MonoBehaviour
+{
 
     public GameObject TextBox;
     public GameObject Choice01;
@@ -11,27 +12,34 @@ public class MuiltpleChoice : MonoBehaviour {
     public GameObject Choice03;
     public int ChoiceMade;
 
-    public void ChoiceOption1 () {
-        TextBox.GetComponent<Text>().text="THIS ONE WHILE MAKE YOUR LIFE GREAT!";
+
+    public void ChoiceOption1()
+    {
+        TextBox.GetComponent<Text>().text = "THIS ONE WHILE MAKE YOUR LIFE GREAT!";
         ChoiceMade = 1;
     }
 
-    public void ChoiceOption2() {
-        TextBox.GetComponent<Text>().text="YOU PICK THE RIGHT ONE!";
+    public void ChoiceOption2()
+    {
+        TextBox.GetComponent<Text>().text = "YOU PICK THE RIGHT ONE!";
         ChoiceMade = 2;
     }
 
-    public void ChoiceOption3() {
-        TextBox.GetComponent<Text>().text="I'M PRETTY SURE THE NEXT ONE WILL MAKE YOU PICK!";
+    public void ChoiceOption3()
+    {
+        TextBox.GetComponent<Text>().text = "I'M PRETTY SURE THE NEXT ONE WILL MAKE YOU PICK!";
         ChoiceMade = 3;
     }
 
-    void Update() {
-        if (ChoiceMade >= 1) {
+    void Update()
+    {
+        if (ChoiceMade >= 1)
+        {
             Choice01.SetActive(false);
             Choice02.SetActive(false);
             Choice03.SetActive(false);
         }
-        
+
     }
 }
+
