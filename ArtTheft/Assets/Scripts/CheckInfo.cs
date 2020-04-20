a37e23bfa85248f4d17c2641a438ca142d5c150a
Jonathan Clement <blowdacon@gmail.com>
2020-04-19 23:07:03 -0400

    Auto stash before merge of "master" and "origin/master"

--- ArtTheft/Assets/Scripts/CheckInfo.cs
+++ ArtTheft/Assets/Scripts/CheckInfo.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class CheckInfo : MonoBehaviour
 {
     public Image image;
+    public Image imageInfo;
     void Start()
     {
        
@@ -13,15 +14,20 @@ public class CheckInfo : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(3)) {
             image = GetComponent<Image>();
             image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+            imageInfo = GetComponent<Image>();
+            imageInfo.color = new Color(image.color.r, image.color.g, image.color.b, 100f);
 
         }
-        if (Input.GetMouseButtonUp(0)) {
+        if (Input.GetMouseButtonUp(3)) {
 
             image = GetComponent<Image>();
             image.color = new Color(image.color.r, image.color.g, image.color.b, 100f);
+            imageInfo = GetComponent<Image>();
+            imageInfo.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+
 
         }
     }
