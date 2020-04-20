e20e961b270ddba89e078598709d6140d3097133
Jonathan Clement <blowdacon@gmail.com>
2020-04-19 22:04:14 -0400

    added a few things. simple changes

---
+++ ArtTheft/Assets/Scripts/CheckInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckInfo : MonoBehaviour
+{
+    public Image image;
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            image = GetComponent<Image>();
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+
+        }
+        if (Input.GetMouseButtonUp(0)) {
+
+            image = GetComponent<Image>();
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 100f);
+
+        }
+    }
+}
