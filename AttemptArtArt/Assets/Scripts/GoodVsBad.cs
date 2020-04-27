e1f6079bec612a1a3133715aa7322da345e2c488
Jonathan Clement <blowdacon@gmail.com>
2020-04-27 00:59:46 -0400

    I believe the project to be complete

---
+++ AttemptArtArt/Assets/Scripts/GoodVsBad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodVsBad : MonoBehaviour
+
+
+{
+    public GameObject Tst;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(Storage.A == 2 || Storage.B == 2 || Storage.B == 2 || Storage.B == 2)
+        {
+            Tst.SetActive(false);
+
+        }
+    }
+
+}
